bafdfdccf60a5a6e2db7c0534307a07fed927553
mohd-asjad <asjadk299@gmail.com>
2023-11-08 19:27:20 +0530

    some changes in code

--- calculator.js
+++ calculator.js
@@ -1,18 +1,22 @@
-let display = document.getElementById('display');
 
-function appendToDisplay(value) {
 
-    display.value =display.value+value;
+function display(value) {
+
+    let display = document.getElementById('display').value+=value;
 }
 
 function clearDisplay() {
-    display.value = '';
+    document.getElementById('display').value='';
 }
 
 function calculateResult() {
-    try {
-        display.value = eval(display.value);
-    } catch (error) {
-        display.value = 'Error';
-    }  
+    // try {
+    //     display.value = eval(display.value);
+    // } catch (error) {
+    //     display.value = 'Error';
+    // }  
+    var value=document.getElementById('display').value;
+    var answer= eval(value);
+    document.getElementById('display').value=answer;
+    
 }
